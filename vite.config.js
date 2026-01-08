cdc59dab53b419fcf48d957a778c9cdf7ec64c3c
EnayatKhn <inayat@aisp.edu.pk>
2026-01-08 20:15:00 +0300

    update Vite configuration for public directory and build settings

--- vite.config.js
+++ vite.config.js
@@ -17,4 +17,9 @@ export default defineConfig({
             '@': '/resources/js/src',
         },
     },
+    publicDir: 'public',
+    build: {
+        outDir: 'public/build',
+        emptyOutDir: true,
+    },
 });
